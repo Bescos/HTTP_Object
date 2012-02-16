0d6d710f5a6d0f8a4c7fb88e2bc9c79e8501c0ac
Tommy Bescos <tommy.bescos@etu.enseeiht.fr>
2012-02-16 19:52:57 +0100

    Creation dune requete avec un socket Double

--- http.rb
+++ http.rb
@@ -1,17 +1,20 @@
 class HTTP
 
  class Request
+  #But de la classe : reconstituer status, headers, body
 
+  def initialize(sock)
+    @socket=sock
+  end
 
-
-
-
-
+  def valid?
+   not @socket.nil?
+  end
 
  end
 
  class Response
-
+#Constituer status, headers, body
 
 
 
--- http_spec.rb
+++ http_spec.rb
@@ -1,13 +1,20 @@
 #Fichier de test de la classe classy_http_debug_server
 $: << File.dirname(__FILE__)
 
-require 'http.rb'
+require 'http'
+
 
 describe HTTP::Request do
 
+context "creation of the Request Object" do
+  it "should create req.sock" do
+    socket = double (Socket)
+    req = HTTP::Request.new(socket)    
+    req.valid?.should be_true
+  end
 
 
-
+end
 end
 
 describe HTTP::Response do
