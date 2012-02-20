ef7d910468afd4003202bfff6708b13213e45f35
Tommy Bescos <tommy.bescos@etu.enseeiht.fr>
2012-02-20 12:52:03 +0100

    Re découpage des tests de la requete

--- http_spec.rb
+++ http_spec.rb
@@ -7,19 +7,27 @@ require 'http'
 describe HTTP::Request do
 
   
-context "creation of the Request Object" do
-  it "should read the request" do
+context "creation of the Request Object with a body" do
+  it "should read the status of the request" do
     socket = double (Socket)
     socket.stub(:gets).and_return("GET / HTTP1.1", "Host: localhost:8080", "Content-Length: 26", "User-Agent: Mozilla", "Accept: text.html", "\r\n")
     socket.stub(:read).and_return("Ici le corps de ma requete")
     req = HTTP::Request.new(socket)  
     req.status.should == "GET / HTTP1.1"
-
-
+ end
+ it "should read the headers of the request" do
+    socket = double (Socket)
+    socket.stub(:gets).and_return("GET / HTTP1.1", "Host: localhost:8080", "Content-Length: 26", "User-Agent: Mozilla", "Accept: text.html", "\r\n")
+    socket.stub(:read).and_return("Ici le corps de ma requete")
+    req = HTTP::Request.new(socket) 
     req.headers["User-Agent"].should == "Mozilla"
     req.headers.length.should == 4
-
-    
+ end
+ it "should read the body of the request" do
+    socket = double (Socket)
+    socket.stub(:gets).and_return("GET / HTTP1.1", "Host: localhost:8080", "Content-Length: 26", "User-Agent: Mozilla", "Accept: text.html", "\r\n")
+    socket.stub(:read).and_return("Ici le corps de ma requete")
+    req = HTTP::Request.new(socket) 
     req.body.should == "Ici le corps de ma requete"
  end
  
